1538524bf17d870606ac08d5a05b8d67a272e9af
Er-Amol <108022258+Er-Amol@users.noreply.github.com>
2022-06-23 00:32:12 +0530

    Add files via upload
    day 1 exercise

---
+++ excercise2 day 1.py
@@ -0,0 +1,13 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+
+#
+print("amol")
+amol
+ca=1993
+ba=1993
+ca=2022
+ag=ca-ba
+print(ag)
+29
