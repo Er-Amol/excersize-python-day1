268495dbe38866122272070e1dbd138479341518
Er-Amol <108022258+Er-Amol@users.noreply.github.com>
2022-06-23 00:35:41 +0530

    Add files via upload
    solutions

---
+++ exez4.py
@@ -0,0 +1,14 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Store your first, middle and last name in three different variables and then print your full name using these variables
+a=amol
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    a=amol
+NameError: name 'amol' is not defined
+a="amol"
+a="amol"
+b="anil"
+c="kadam"
+print(a ,b , c)
+amol anil kadam
